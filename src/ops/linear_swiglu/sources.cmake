8bbd0365fe6d4742ab8ffb68e4ff664e79a2b2f3
ww485000 <162346442+ww485000@users.noreply.github.com>
2026-09-25 19:06:26 +0800

    Exclude NVFP4 TMA SwiGLU kernel from Volta build

--- src/ops/linear_swiglu/sources.cmake
+++ src/ops/linear_swiglu/sources.cmake
@@ -18,6 +18,8 @@ target_sources(ninfer_ops PRIVATE
   "${CMAKE_CURRENT_LIST_DIR}/../wrapper/linear_swiglu.cpp"
 )
 
-target_sources(ninfer_nvfp4_non_rdc PRIVATE
-  "${CMAKE_CURRENT_LIST_DIR}/nvfp4/nvfp4_linear_swiglu_w4a4_tma.cu"
-)
+if(NOT CMAKE_CUDA_ARCHITECTURES STREQUAL "70")
+  target_sources(ninfer_nvfp4_non_rdc PRIVATE
+    "${CMAKE_CURRENT_LIST_DIR}/nvfp4/nvfp4_linear_swiglu_w4a4_tma.cu"
+  )
+endif()
