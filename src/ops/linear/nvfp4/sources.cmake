7eb49fd53c077116141fa4c606eea0a0093040b8
ww485000 <162346442+ww485000@users.noreply.github.com>
2026-09-25 19:06:24 +0800

    Exclude NVFP4 TMA linear kernel from Volta build

--- src/ops/linear/nvfp4/sources.cmake
+++ src/ops/linear/nvfp4/sources.cmake
@@ -10,6 +10,8 @@ target_sources(ninfer_ops PRIVATE
   "${CMAKE_CURRENT_LIST_DIR}/shapes/n5120_k17408.cu"
 )
 
-target_sources(ninfer_nvfp4_non_rdc PRIVATE
-  "${CMAKE_CURRENT_LIST_DIR}/nvfp4_w4a4_tma.cu"
-)
+if(NOT CMAKE_CUDA_ARCHITECTURES STREQUAL "70")
+  target_sources(ninfer_nvfp4_non_rdc PRIVATE
+    "${CMAKE_CURRENT_LIST_DIR}/nvfp4_w4a4_tma.cu"
+  )
+endif()
