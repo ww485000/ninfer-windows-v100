3fa1c07bb85a3e7e47ded3e0bd2afc0e9d254d05
ww485000 <162346442+ww485000@users.noreply.github.com>
2026-09-25 19:16:54 +0800

    Batch-gate SM75+ projection/materialize kernels from Volta build

--- src/ops/context_kv_materialize/sources.cmake
+++ src/ops/context_kv_materialize/sources.cmake
@@ -1,6 +1,11 @@
 target_sources(ninfer_ops PRIVATE
   "${CMAKE_CURRENT_LIST_DIR}/context_kv_materialize.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/materialize.cu"
   "${CMAKE_CURRENT_LIST_DIR}/context_kv_key_post.cu"
-  "${CMAKE_CURRENT_LIST_DIR}/materialize_nvfp4.cu"
 )
+
+if(NOT CMAKE_CUDA_ARCHITECTURES STREQUAL "70")
+  target_sources(ninfer_ops PRIVATE
+    "${CMAKE_CURRENT_LIST_DIR}/materialize.cu"
+    "${CMAKE_CURRENT_LIST_DIR}/materialize_nvfp4.cu"
+  )
+endif()
